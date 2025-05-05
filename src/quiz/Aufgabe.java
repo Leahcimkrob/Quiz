6f9ff0fe99a805a10e09d4ba770e5ad6769ed6bb
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-05 15:11:06 +0200

    Inhalte hinzugefügt

--- src/quiz/Aufgabe.java
+++ src/quiz/Aufgabe.java
@@ -1,12 +1,16 @@
 package quiz;
-	/**
+
+import java.util.Scanner;
+
+/**
 	 * Definiert die Struktur für alle Aufgaben:
 	 * - Ausgabe
 	 * - Eingabe
 	 * - Prüfung
 	 */
 	public interface Aufgabe {
-	    void stelleFrage();
-	    void leseAntwort();
-	    void pruefeAntwort();
+		public void initialiereFrage();
+	    public void stelleFrage();
+	    public String leseAntwort(Scanner scanner);
+	    public void pruefeAntwort(String antwort);
 }
