3c96e6fa97ffcc6d5723477d2d49aca31d26f834
Michael Bork <michael.bork@rwofan.de>
2025-05-07 11:50:58 +0200

    update

--- src/quiz/MCFragenBeantwortenUI.java
+++ src/quiz/MCFragenBeantwortenUI.java
@@ -34,14 +34,15 @@ public class MCFragenBeantwortenUI extends JPanel {
 		textField2 = new JTextField();
 
 		//======== this ========
-		setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.
-		EmptyBorder(0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion",javax.swing.border.TitledBorder.CENTER,javax.swing
-		.border.TitledBorder.BOTTOM,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12),
-		java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener()
-		{@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062order".equals(e.getPropertyName()))
-		throw new RuntimeException();}});
+		setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
+		. swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing
+		.border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
+		Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
+		) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
+		public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062order" .equals ( e. getPropertyName (
+		) ) )throw new RuntimeException( ) ;} } );
 		setLayout(new MigLayout(
-			"hidemode 3",
+			"hidemode 3,alignx center",
 			// columns
 			"[234,fill]",
 			// rows
