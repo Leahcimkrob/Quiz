c6dd72c4d7146dc81de8796f0078fe000dd96d5f
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-08 16:53:36 +0200

    .

--- src/quiz/AufgabenTrainerUI.java
+++ src/quiz/AufgabenTrainerUI.java
@@ -1,13 +1,7 @@
 package quiz;
 
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class AufgabenTrainerUI extends JFrame {
     private JPanel panelStart;
@@ -15,49 +9,61 @@ public class AufgabenTrainerUI extends JFrame {
     private JPanel panelJavaCode;
     private JPanel contentPanel;
 
-
     public AufgabenTrainerUI() {
         setTitle("App mit Menü");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800, 640);
 
         // Inhalte vorbereiten
+        contentPanel = new JPanel(); // Initialisiere contentPanel
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        add(contentPanel); // Füge contentPanel zum JFrame hinzu
+
         panelStart = new JPanel();
         panelStart.add(new JLabel("tbd"));
 
         panelFragen = new JPanel();
         panelFragen.add(new JLabel("Startseite"));
 
-        panelJavaCode = new JavaFrageAnzeigeUI();
+        panelJavaCode = new JavaCheckerUI();
 
-
-        setJMenuBar(createMenu());
+        setJMenuBar(createMenuBar());
 
         showPanel(panelStart);
         setVisible(true);
     }
 
-    private JMenuBar createMenu() {
+    private JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
-        JMenu menu = new JMenu("Navigation");
+        // Erstes Menü: Navigation
+        JMenu navigationMenu = new JMenu("Navigation");
         JMenuItem itemStart = new JMenuItem("Start");
         JMenuItem itemFragen = new JMenuItem("MC-Frage");
         JMenuItem itemEinstellungen = new JMenuItem("Programmieraufgabe");
-        JMenuItem itemFragenNeu = new JMenuItem("Neue MC-Frage");
-              
-        
+
         itemStart.addActionListener(e -> showPanel(panelStart));
         itemFragen.addActionListener(e -> showPanel(panelFragen));
         itemEinstellungen.addActionListener(e -> showPanel(panelJavaCode));
-        
 
-        menu.add(itemStart);
-        menu.add(itemFragen);
-        menu.add(itemEinstellungen);
-        menu.add(itemFragenNeu);
-        
-        menuBar.add(menu);
+        navigationMenu.add(itemStart);
+        navigationMenu.add(itemFragen);
+        navigationMenu.add(itemEinstellungen);
+
+        // Zweites Menü: Optionen
+        JMenu optionsMenu = new JMenu("Bearbeiten");
+        JMenuItem option1 = new JMenuItem("Option 1");
+        JMenuItem option2 = new JMenuItem("Code erstellen");
+
+        option1.addActionListener(e -> JOptionPane.showMessageDialog(this, "Option 1 ausgewählt."));
+        option2.addActionListener(e -> JOptionPane.showMessageDialog(this, "Option 2 ausgewählt."));
+
+        optionsMenu.add(option1);
+        optionsMenu.add(option2);
+
+        menuBar.add(navigationMenu);
+        menuBar.add(optionsMenu);
+
         return menuBar;
     }
 
