7cb7bb7bc0ae1169c65d4dd7ceeb7f207c88eae5
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-08 15:50:06 +0200

    Updates

--- src/quiz/JavaFrageAnzeigeUI.java
+++ src/quiz/JavaFrageAnzeigeUI.java
@@ -16,21 +16,24 @@ public class JavaFrageAnzeigeUI extends JPanel {
     private void initAufgabe() {
         // Rufe die Methode getAlleFragenUndAntworten auf, um die Daten zu erhalten
         List<String[]> fragenListe = fragenManager.getAlleFragenUndAntworten();
-
+       int zeile = 1; 
         for (String[] frageDetails : fragenListe) {
             // Für jede Nummer, Frage und Antwort eine Zeile erstellen
+        	JButton button = new JButton("Bearb.");
             JCheckBox checkBox = new JCheckBox(frageDetails[0]); // Checkbox für die Nummer
             JTextField textFieldFrage = new JTextField(frageDetails[1]); // Textfeld für die Frage
             JTextField textFieldAntwort = new JTextField(frageDetails[2]); // Textfeld für die Antwort
 
-            // Setze feste Breite für die Textfelder
-            textFieldFrage.setColumns(30);
-            textFieldAntwort.setColumns(15);
+//            // Setze feste Breite für die Textfelder
+//            textFieldFrage.setColumns(30);
+//            textFieldAntwort.setColumns(15);
 
             // Dynamisch die Komponenten zur GUI hinzufügen
-            add(checkBox, "cell 0 " + (getComponentCount() / 3 + 1) + ",alignx left"); // Spalte 0
-            add(textFieldFrage, "cell 1 " + (getComponentCount() / 3 + 1) + ",growx"); // Spalte 1
-            add(textFieldAntwort, "cell 2 " + (getComponentCount() / 3 + 1) + ",growx"); // Spalte 2
+            add(button, "cell 0 " + (zeile) + ",alignx left"); // Spalte 0
+            add(checkBox, "cell 1 " + (zeile) + ",alignx left"); // Spalte 0
+            add(textFieldFrage, "cell 2 " + (zeile) + ",growx"); // Spalte 1
+            add(textFieldAntwort, "cell 3 " + (zeile) + ",growx"); // Spalte 2
+            zeile++;
         }
 
         // Nach dem Hinzufügen von Komponenten die GUI neu validieren
@@ -39,20 +42,52 @@ public class JavaFrageAnzeigeUI extends JPanel {
     }
 
     private void initComponents() {
-        // Komponenten-Initialisierung
-        setLayout(new MigLayout(
-            "hidemode 3",
-            // Spalten
-            "[grow,fill]" +
-            "[grow,fill]" +
-            "[grow,fill]",
-            // Zeilen
-            "[]15[]" // Fügt Abstand zwischen den Zeilen ein
-        ));
 
-        // Überschrift hinzufügen
-        JLabel label1 = new JLabel("Fragenübersicht");
-        add(label1, "cell 0 0 3 1,alignx center");
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+		// Generated using JFormDesigner Evaluation license - Michael Bork
+		label1 = new JLabel();
+		button2 = new JButton();
 
-    }
+		//======== this ========
+		setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new
+		javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax
+		.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java
+		.awt.Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt
+		.Color.red), getBorder())); addPropertyChangeListener(new java.beans.
+		PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062or\u0064er".
+		equals(e.getPropertyName()))throw new RuntimeException();}});
+		setLayout(new MigLayout(
+			"hidemode 3",
+			// columns
+			"[13,fill]" +
+			"[100:41:41,fill]" +
+			"[194:258,grow,fill]" +
+			"[100:230:100,grow,fill]" +
+			"[fill]" +
+			"[fill]",
+			// rows
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]"));
+
+		//---- label1 ----
+		label1.setText("Fragen\u00fcbersicht");
+		add(label1, "cell 0 0 4 1,alignx center,growx 0");
+
+		//---- button2 ----
+		button2.setText("Markierte L\u00f6schen");
+		add(button2, "cell 0 7 6 1,alignx center,growx 0");
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+	// Generated using JFormDesigner Evaluation license - Michael Bork
+	private JLabel label1;
+	private JButton button2;
+	// JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
