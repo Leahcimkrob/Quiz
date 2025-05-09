e984660e21051e12b2d764c61bb292bd884cca07
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-09 15:37:29 +0200

    .

--- src/quiz/MCFragenBeantwortenUI.java
+++ src/quiz/MCFragenBeantwortenUI.java
@@ -37,12 +37,11 @@ public class MCFragenBeantwortenUI extends JPanel {
 		label3 = new JLabel();
 
 		//======== this ========
-		setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border
-		.EmptyBorder(0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border.TitledBorder.CENTER,javax
-		.swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,
-		12),java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans
-		.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062ord\u0065r".equals(e.
-		getPropertyName()))throw new RuntimeException();}});
+		setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
+		0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
+		.BOTTOM,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12),java.awt.Color.
+		red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
+		beans.PropertyChangeEvent e){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException();}});
 		setLayout(new MigLayout(
 			"hidemode 3,alignx center",
 			// columns
@@ -90,7 +89,7 @@ public class MCFragenBeantwortenUI extends JPanel {
 		add(button2, "cell 0 6,alignx right,growx 0,width 111:111");
 
 		//---- label3 ----
-		label3.setText("Richtig/Falsch");
+		label3.setText("RichtigFalsch");
 		add(label3, "cell 0 7,alignx center,growx 0");
 
 		//---- buttonGroup1 ----
